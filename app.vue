c3c3ad9f77bff04756a8d00439cd524488c557f7
szig83 <35454811+szig83@users.noreply.github.com>
2022-06-28 23:14:31 +0200

    Add files via upload

---
+++ app.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { Pool } from "pg";
+
+const db = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "postgres",
+  password: "12345",
+  port: 5432,
+  max: 10,
+})
+
+const connectToDB = async () => {
+  try {
+    await db.connect();
+  } catch (err) {
+    console.log(err);
+  }
+};
+connectToDB();
+</script>
+
+<template>
+  <div>
+  </div>
+</template>
